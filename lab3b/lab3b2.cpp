28b04cd68337540402189c051c7d81c4f53a6c83
Jack Fenton <mllax8@me.com>
2020-02-06 15:30:29 -0500

    Has no way to run but object inheritence created

--- lab3b/lab3b2.cpp
+++ lab3b/lab3b2.cpp
@@ -9,7 +9,7 @@
 //Jack Fenton
 //Northeastern University
 //EECE2150 Embedded Design
-//Lab 3
+//Lab 3b Object Oriented
 //30 January 2020
 
 
@@ -23,6 +23,7 @@ const unsigned gpio_address = 0x400d0000;
 // Length of memory-mapped IO window
 const unsigned gpio_size = 0xff;
 
+/*
 const int gpio_led1_offset = 0x12C; // Offset for LED1
 const int gpio_led2_offset = 0x130; // Offset for LED2
 const int gpio_led3_offset = 0x134; // Offset for LED3
@@ -44,37 +45,56 @@ const int gpio_pbtnr_offset = 0x170; // Offset for right push button
 const int gpio_pbtnu_offset = 0x174; // Offset for up push button
 const int gpio_pbtnd_offset = 0x178; // Offset for down push button
 const int gpio_pbtnc_offset = 0x17C; // Offset for center push button
-
+*/
 
 class ZedBoard()
 {
 private:
-int fd;
-
+	static int fd;
+	static char *pBase;
 
 protected:
-char *pBase;
 
 
 public:
 
-ZedBoard();
-~ZedBoard();
+	ZedBoard();
+	~ZedBoard();
 
+	void RegisterWrite(int offset, int value);
+	int RegisterRead(int offset);
 
-void RegisterWrite(int offset, int value);
-int RegisterRead(int offset);
+};
 
-}
-
+/*
+   Initialize general-purpose I/O
+ * - Opens access to physical memory /dev/mem
+ * - Maps memory at offset 'gpio_address' into virtual address space
+ *
+ * @param fd File descriptor passed by reference, where the result
+ * of function 'open' will be stored.
+ * @return Address to virtual memory which is mapped to physical,
+ * or MAP_FAILED on error.
+ */
 ZedBoard::ZedBoard()
 {
 	fd = open( "/dev/mem", O_RDWR);
 	*pBase = (char *) mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED,
 			*fd, gpio_address);
+
+	if (pBase == MAP_FAILED)
+	{
+		cerr << "Mapping I/O memory failed - Did you run with 'sudo'?\n";
+		exit(1); // Returns 1 to the operating system;
+	}
 }
 
-
+/*
+ * Close general-purpose I/O.
+ *
+ * @param pBase Virtual address where I/O was mapped.
+ * @param fd File descriptor previously returned by 'open'.
+ */
 ~ZedBoard::ZedBoard()
 {
 	munmap(pBase, gpio_size);
@@ -89,7 +109,7 @@ ZedBoard::ZedBoard()
  * @parem offset Offset where device is mapped.
  * @param value Value to be written.
  */ 
-void RegisterWrite(int offset, int value)
+void ZedBoard::RegisterWrite(int offset, int value)
 {
 	* (int *) (pBase + offset) = value;
 } 
@@ -102,7 +122,7 @@ void RegisterWrite(int offset, int value)
  * @param offset Offset where device is mapped.
  * @return Value read.
  */
-int RegisterRead(int offset)
+int ZedBoard::RegisterRead(int offset)
 {
 	return * (int *) (pBase + offset);
 }
@@ -112,37 +132,33 @@ int RegisterRead(int offset)
 
 
 /*  Initialize Functions  */
+/*
 char *Initialize(int *fd);
 void Finalize(char *pBase, int fd);
-void RegisterWrite(char *pBase, int offset, int value);
-int RegisterRead(char *pBase, int offset);
+
 void Write1Led(char *pBase, int ledNum, int state);
 int Read1Led(char *pBase, int ledNum);
-void Write1Switch(char *pBase, int switchNum, int state);
-int Read1Switch(char *pBase, int switchNum);
 void WriteAllLeds(char *pBase, int numLEDs);
-int isInt (string prompt, int min, int max);
-int ReadAllSwitchs(char *pBase);
 int ReadAllLeds(char *pBase);
+
+int Read1Switch(char *pBase, int switchNum);
+int ReadAllSwitches(char *pBase);
+
 void ButtonCommands(char *pBase);
 int PushButtonGet(char *pBase);
+*/
 
+int isInt (string prompt, int min, int max);
 
 
 int main()
 {
-	// Initialize
-	int fd;
-	char *pBase = Initialize(&fd);
+	
 	int cur_case;
 	int numIn, stateIn, stateOut,numOut;
 
-	// Check error
-	if (pBase == MAP_FAILED)
-	{
-		cerr << "Mapping I/O memory failed - Did you run with 'sudo'?\n";
-		exit(1); // Returns 1 to the operating system;
-	}
+
+	
 
 	while(cur_case!=6) 
 
@@ -166,6 +182,8 @@ int main()
 		{
 			case 1:
 				//Change LED
+
+
 				numIn=isInt("Choose an LED to set: ",0,7);
 				stateIn=isInt("Choose state for the LED: ",0,1);
 				Write1Led(pBase, numIn, stateIn);
@@ -174,6 +192,8 @@ int main()
 
 			case 2:
 				//Read Switch
+
+
 
 				numIn=isInt("Choose a Switch to read: ",0,7);
 				stateOut=Read1Switch(pBase, numIn);
@@ -191,7 +211,7 @@ int main()
 			case 4:
 
 
-				numOut=ReadAllSwitchs(pBase);
+				numOut=ReadAllSwitches(pBase);
 				cout<<"The 8 bit number made by the switches is "<<numOut<<endl;
 
 
@@ -205,146 +225,8 @@ int main()
 				cout << "Exit \n\n";
 
 		}
-
 	}
-
-	// Done
-	Finalize(pBase, fd);
 }
-
-
-
-/*
- * Write a 4-byte value at the specified general-purpose I/O location.
- * 
- * @param pBase Base address returned by 'mmap'.
- * @parem offset Offset where device is mapped.
- * @param value Value to be written.
- */ 
-void RegisterWrite(char *pBase, int offset, int value)
-{
-	* (int *) (pBase + offset) = value;
-} 
-
-
-/*
- * Read a 4-byte value from the specified general-purpose I/O location.
- * 
- * @param pBase Base address returned by 'mmap'.
- * @param offset Offset where device is mapped.
- * @return Value read.
- */
-int RegisterRead(char *pBase, int offset)
-{
-	return * (int *) (pBase + offset);
-} 
-
-
-
-/*
-   Initialize general-purpose I/O
- * - Opens access to physical memory /dev/mem
- * - Maps memory at offset 'gpio_address' into virtual address space
- *
- * @param fd File descriptor passed by reference, where the result
- * of function 'open' will be stored.
- * @return Address to virtual memory which is mapped to physical,
- * or MAP_FAILED on error.
- */
-char *Initialize(int *fd)
-{
-	*fd = open( "/dev/mem", O_RDWR);
-	return (char *) mmap(NULL, gpio_size, PROT_READ | PROT_WRITE, MAP_SHARED,
-			*fd, gpio_address);
-} 
-
-
-/*
- * Close general-purpose I/O.
- *
- * @param pBase Virtual address where I/O was mapped.
- * @param fd File descriptor previously returned by 'open'.
- */
-void Finalize(char *pBase, int fd)
-{
-	munmap(pBase, gpio_size);
-	close(fd);
-} 
-
-
-/* Changes the state of an LED (ON or OFF)
- * @param pBase base address of I/O
- * @param ledNum LED number (0 to 7)
- * @param state State to change to (ON or OFF)
- */
-void Write1Led(char *pBase, int ledNum, int state)
-{
-	int ledNum_offset = gpio_led1_offset + 4*ledNum;
-	RegisterWrite(pBase, ledNum_offset, state);
-}
-
-/* Reads the value of an LED
- * - Uses base address of I/O
- * @param pBase base address of I/O
- * @param ledNum LED number (0 to 7)
- * @return LED value read
- */
-int Read1Led(char *pBase, int ledNum)
-{
-	int ledNum_offset = gpio_led1_offset + 4*ledNum;
-	return RegisterRead(pBase, ledNum_offset);
-}
-
-
-
-/* Reads the value of a switch
- * - Uses base address of I/O
- * @param pBase base address of I/O
- * @param switchNum Switch number (0 to 7)
- * @return Switch value read
- */
-int Read1Switch(char *pBase, int switchNum)
-{
-	int switchNum_offset = gpio_sw1_offset + 4*switchNum;
-	return RegisterRead(pBase, switchNum_offset);
-}
-
-
-
-
-void WriteAllLeds(char *pBase, int numLEDs)
-{
-	for (int i=0;i<=7;i++) 
-	{
-		Write1Led(pBase, i, numLEDs%2);
-		numLEDs/=2;
-	}
-
-}
-
-
-int ReadAllSwitchs(char *pBase)
-{
-	int tempInt=0;
-	for (int i=0;i<=7;i++)
-	{
-		tempInt+=Read1Switch(pBase,i)*(int)pow(2,i);
-	}
-	return tempInt; 
-}
-
-
-int ReadAllLeds(char *pBase)
-{
-	int tempInt=0;
-	for (int i=0;i<=7;i++)
-	{
-		tempInt+=Read1Led(pBase,i)*(int)pow(2,i);
-	}
-	return tempInt; 
-
-}
-
 
 /*  Prints a prompt and takes input, returns input when it is int between min and max */
 int isInt(string prompt, int min, int max)
@@ -365,23 +247,145 @@ int isInt(string prompt, int min, int max)
 }
 
 
-int Read1Butt(char *pBase, int buttNum)
+
+class LEDs : public ZedBoard
 {
-	int buttNum_offset = gpio_pbtnl_offset + 4*buttNum;
-	return RegisterRead(pBase, buttNum_offset);
+private:
+	static int LED_base = 0x12C;
+
+
+public:
+	LEDs(){}
+	~LEDs(){}
+
+	void Write1Led(int ledNum, int state);
+	void WriteAllLeds(int numLEDs);
+
+	int Read1Led(int ledNum);
+	int ReadAllLeds();
+};
+
+
+/* Changes the state of an LED (ON or OFF)
+ * @param ledNum LED number (0 to 7)
+ * @param state State to change to (ON or OFF)
+ */
+void LEDs::Write1Led(int ledNum, int state)
+{
+	int ledNum_offset = LED_base + 4*ledNum;
+	RegisterWrite(ledNum_offset, state);
+}
+
+void LEDs::WriteAllLeds(int numLEDs)
+{
+	for (int i=0;i<=7;i++) 
+	{
+		Write1Led(i, numLEDs%2);
+		numLEDs/=2;
+	}
+}
+
+/* Reads the value of an LED
+ * - Uses base address of I/O
+ * @param ledNum LED number (0 to 7)
+ * @return LED value read
+ */
+int LEDs::Read1Led(int ledNum)
+{
+	int ledNum_offset = LED_base + 4*ledNum;
+	return RegisterRead(ledNum_offset);
+}
+
+int LEDs::ReadAllLeds()
+{
+	int tempInt=0;
+	for (int i=0;i<=7;i++)
+	{
+		tempInt+=Read1Led(i)*(int)pow(2,i);
+	}
+	return tempInt; 
 }
 
 
-void ButtonCommands(char *pBase)
+
+class Switches : public ZedBoard
+{
+private:
+	static int Switch_base = 0x14C;
+
+protected:
+
+public:
+	Switches(){}
+	~Switches(){}
+
+
+	int Read1Switch(int switchNum);
+	int ReadAllSwitches();
+};
+
+
+
+/* Reads the value of a switch
+ * - Uses base address of I/O
+ * @param switchNum Switch number (0 to 7)
+ * @return Switch value read
+ */
+int Switches::Read1Switch(int switchNum)
+{
+	int switchNum_offset = Switch_base + 4*switchNum;
+	return RegisterRead(switchNum_offset);
+}
+
+
+int Switches::ReadAllSwitches()
+{
+	int tempInt=0;
+	for (int i=0;i<=7;i++)
+	{
+		tempInt+=Read1Switch(i)*(int)pow(2,i);
+	}
+	return tempInt;
+}
+
+
+
+class Buttons : public ZedBoard
+{
+private:
+	static int Button_base = 0x16C;
+
+protected:
+
+public:
+	Buttons(){}
+	~Buttons(){}
+
+
+	int Read1Butt(int buttNum);
+	int PushButtonGet();
+	void ButtonCommands();
+};
+
+
+
+int Buttons::Read1Butt(int buttNum)
+{
+	int buttNum_offset = Button_base + 4*buttNum;
+	return RegisterRead(buttNum_offset);
+}
+
+
+void Buttons::ButtonCommands()
 {
 	int cur_butt=0,last_butt=0,count=0;	
 
-	WriteAllLeds(pBase,ReadAllSwitchs(pBase));
-	count=ReadAllSwitchs(pBase);
+	count=ReadAllSwitches();
+	WriteAllLeds(count);
 	while(true)
 	{	
 		//Getting Current Button		
-		cur_butt=PushButtonGet(pBase);		
+		cur_butt=PushButtonGet();		
 		usleep(100000);
 		if (cur_butt!=last_butt)
 		{
@@ -390,32 +394,32 @@ void ButtonCommands(char *pBase)
 				case 1:
 					//Shift Digits Right
 					count*=2;
-					WriteAllLeds(pBase, count);
+					WriteAllLeds(count);
 					break;
 
 				case 2:
 					//Shift Digits Left
 					count/=2;
-					WriteAllLeds(pBase, count);
+					WriteAllLeds(count);
 					break;
 
 				case 3:
 					//Incriment LEDS +1
 					count++;
-					WriteAllLeds(pBase, count);
-					ReadAllLeds(pBase);
+					WriteAllLeds(count);
+					ReadAllLeds();
 					break;
 
 				case 4:
 					//Incriment LEDS -1
 					count--;
-					WriteAllLeds(pBase, count);
+					WriteAllLeds(count);
 					break;
 
 				case 5:
 
 					//Set LEDs to Switches Num
-					WriteAllLeds(pBase,ReadAllSwitchs(pBase));
+					WriteAllLeds(ReadAllSwitches());
 					break;
 				case 6:
 					//exit
@@ -427,15 +431,16 @@ void ButtonCommands(char *pBase)
 	}
 }
 
-int PushButtonGet(char *pBase)
+int Buttons::PushButtonGet()
 {
 	int numPress=0;
 	for(int i=0; i<5;i++)
 	{	
-		numPress+=(i+1)*Read1Butt(pBase,i);
+		numPress+=(i+1)*Read1Butt(i);
 	}
 	return numPress;
 }
+
 
 
 
